9a869403a9870e60167af64dde43cc735c9d9089
tqy <tangqianyu123456@gmail.com>
2019-10-04 16:11:02 +0800

    no message

--- src/app/tools/image-convert-text/image-convert-text.component.ts
+++ src/app/tools/image-convert-text/image-convert-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, AfterViewInit, ViewChild, ElementRef, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'image-convert-text',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageConvertTextComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('photo', { static: false })
+  photo: ElementRef
+
+
+
+  constructor(
+    private render2: Renderer2
+  ) {
+
+  }
 
   ngOnInit() {
+  }
+
+  fileChange(e) {
+    let files = e.srcElement.files
+    /* 展示预览图片 */
+    for (let item of files) {
+      let addDiv = this.render2.createElement('div')
+      let addImg = this.render2.createElement('img')
+      this.render2.addClass(addImg, 'qy_addImg')
+      this.render2.setAttribute(addImg, 'src', window.URL.createObjectURL(item))
+      this.render2.appendChild(addDiv, addImg)
+      this.render2.appendChild(this.photo.nativeElement, addDiv)
+    }
   }
 
 }
